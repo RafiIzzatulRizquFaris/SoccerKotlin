f979975b6bbf6e6caf125a9dac06908fdf6d187a
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-12-28 06:47:51 +0700

    Update UserService.kt

--- app/src/main/java/com/example/submission3kotlin/retrofit/UserService.kt
+++ app/src/main/java/com/example/submission3kotlin/retrofit/UserService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface UserService {
     @GET("lookupleague.php")
     fun responseDetailLeague(
-        @Query("id") id: Int
+        @Query("id") id: String
     ): Observable<DetailLeague>
 
     @GET("eventspastleague.php")
