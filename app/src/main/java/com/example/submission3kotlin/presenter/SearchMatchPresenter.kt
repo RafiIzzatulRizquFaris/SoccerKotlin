2799f04119191c4a90de83d37ece7709c5cee208
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-02-09 08:03:51 +0700

    Update SearchMatchPresenter.kt

--- app/src/main/java/com/example/submission3kotlin/presenter/SearchMatchPresenter.kt
+++ app/src/main/java/com/example/submission3kotlin/presenter/SearchMatchPresenter.kt
@@ -1,22 +1,20 @@
 package com.example.submission3kotlin.presenter
 
 import android.util.Log
+import com.example.submission3kotlin.DataRepositoryImpl
+import com.example.submission3kotlin.SchedulerProvider
 import com.example.submission3kotlin.contract.SearchMatchContract
-import com.example.submission3kotlin.retrofit.RetrofitClient
-import com.example.submission3kotlin.retrofit.UserService
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 
-class SearchMatchPresenter(val view: SearchMatchContract.View) : SearchMatchContract.Presenter {
+class SearchMatchPresenter(val view: SearchMatchContract.View,
+                           private val scheduler: SchedulerProvider,
+                           private val request: DataRepositoryImpl
+) : SearchMatchContract.Presenter {
     override fun getSearchMatch(strQuery: String?) {
-        Log.e("TAG", strQuery.toString())
-        val retrofit = RetrofitClient.getClient()
-            .create(UserService::class.java)
         CompositeDisposable().add(
-            retrofit.responseSearch(strQuery.toString())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
+            request.getSearch(strQuery.toString())
+                .observeOn(scheduler.ui())
+                .subscribeOn(scheduler.io())
                 .subscribe(
                     {
                         view.setSearchMatch(it.searchevent)
