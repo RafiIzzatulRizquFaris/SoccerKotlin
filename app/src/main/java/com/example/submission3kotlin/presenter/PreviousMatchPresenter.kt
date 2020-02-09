9e5d2b9251cc15fa763bb7f368cc153cd4a275be
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-02-09 08:03:57 +0700

    Update PreviousMatchPresenter.kt

--- app/src/main/java/com/example/submission3kotlin/presenter/PreviousMatchPresenter.kt
+++ app/src/main/java/com/example/submission3kotlin/presenter/PreviousMatchPresenter.kt
@@ -1,22 +1,20 @@
 package com.example.submission3kotlin.presenter
 
 import android.util.Log
+import com.example.submission3kotlin.DataRepositoryImpl
+import com.example.submission3kotlin.SchedulerProvider
 import com.example.submission3kotlin.contract.PreviousMatchContract
-import com.example.submission3kotlin.retrofit.RetrofitClient
-import com.example.submission3kotlin.retrofit.UserService
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 
-class PreviousMatchPresenter(val view: PreviousMatchContract.View) :
-    PreviousMatchContract.Presenter {
+class PreviousMatchPresenter(val view: PreviousMatchContract.View,
+                             private val scheduler: SchedulerProvider,
+                             private val request: DataRepositoryImpl
+) : PreviousMatchContract.Presenter {
     override fun getPreviousMatch(league: String?) {
-        val retrofit = RetrofitClient.getClient()
-            .create(UserService::class.java)
         CompositeDisposable().add(
-            retrofit.responsePastLeague(league.toString())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
+            request.getPastLeague(league.toString())
+                .observeOn(scheduler.ui())
+                .subscribeOn(scheduler.io())
                 .subscribe(
                     {
                         view.setPreviousMatch(it.events)
