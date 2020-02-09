af754163ea5a3f30aa5378b9d081fa06b1adf996
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-02-09 08:03:30 +0700

    Update DetailLeagueTest.kt

--- app/src/test/java/com/example/submission3kotlin/DetailLeagueTest.kt
+++ app/src/test/java/com/example/submission3kotlin/DetailLeagueTest.kt
@@ -4,42 +4,45 @@ import com.example.submission3kotlin.contract.DetailLeagueContract
 import com.example.submission3kotlin.model.DetailLeague
 import com.example.submission3kotlin.model.League
 import com.example.submission3kotlin.presenter.DetailLeaguePresenter
-import com.example.submission3kotlin.retrofit.RetrofitClient
-import com.example.submission3kotlin.retrofit.UserService
 import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class DetailLeagueTest {
-        @Mock
-        lateinit var view: DetailLeagueContract.View
+    @Mock
+    lateinit var view: DetailLeagueContract.View
 
-        @Mock
-        private val retrofitClient = RetrofitClient.getClient().create(UserService::class.java)
+    @Mock
+    lateinit var dataRepositoryImpl: DataRepositoryImpl
 
-        private lateinit var mPresenter: DetailLeaguePresenter
+    @Mock
+    lateinit var scheduler: SchedulerProvider
 
-        private lateinit var league: DetailLeague
+    private lateinit var mPresenter: DetailLeaguePresenter
 
-        private lateinit var detailLeague: Observable<DetailLeague>
+    private lateinit var league: DetailLeague
 
-        private val leagues = mutableListOf<League>()
+    private lateinit var detailLeague: Observable<DetailLeague>
 
-        @Before
-        fun setUp(){
-            MockitoAnnotations.initMocks(this)
-            league = DetailLeague(leagues)
-            detailLeague = Observable.just(league)
-            mPresenter = DetailLeaguePresenter(view)
-            Mockito.`when`(retrofitClient.responseDetailLeague("4328")).thenReturn(detailLeague)
-        }
+    private val leagues: MutableList<League> = mutableListOf()
 
-        @Test
-        fun getDetailLeague() {
-            mPresenter.getDetailLeague("4328")
-            Mockito.verify(view).setDetailLeague(leagues)
-        }
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        scheduler = TestSchedulerProvider()
+        league = DetailLeague(leagues)
+        detailLeague = Observable.just(league)
+        mPresenter = DetailLeaguePresenter(view, scheduler, dataRepositoryImpl)
+        `when`(dataRepositoryImpl.getDetailLeague("4328")).thenReturn(detailLeague)
+    }
+
+    @Test
+    fun getDetailLeague() {
+        mPresenter.getDetailLeague("4328")
+        Mockito.verify(view).setDetailLeague(leagues)
+    }
 }
