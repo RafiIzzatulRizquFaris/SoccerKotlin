9784560b7e12459d190991160ea0ba6d48ee6ccf
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-02-09 08:03:05 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/submission3kotlin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/submission3kotlin/ui/activity/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.submission3kotlin.ui.activity
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.submission3kotlin.R
 import com.example.submission3kotlin.adapter.HomeTabAdapter
@@ -21,5 +26,25 @@ class MainActivity : AppCompatActivity() {
             )
         )
         main_tab_layout.setupWithViewPager(main_view_pager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu!!.findItem(R.id.search).actionView as android.widget.SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = "Search Your Team"
+        searchView.setOnQueryTextListener(object : OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val intent = Intent(this@MainActivity, SearchTeamActivity::class.java)
+                intent.putExtra("query_team", query)
+                startActivity(intent)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean = false
+        })
+        return true
     }
 }
